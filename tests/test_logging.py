d6adff1c50d727e9593683083eb43acaa4224e2d
juju4 <juju4@users.noreply.github.com>
2026-05-24 19:33:41 +0000

    fix: test_logging (3)

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,10 +1,9 @@
-from pyinfra import host as pyinfra_host
 from pyinfra.facts.server import LinuxDistribution
 
 
 def test_syslog_file(host):
     syslog_f = host.file("/etc/default/syslog")
-    if pyinfra_host.get_fact(LinuxDistribution)["name"] == "Buildroot":
+    if host.get_fact(LinuxDistribution)["name"] == "Buildroot":
         assert syslog_f.contains("-L -R")
         assert syslog_f.user == "root"
         assert syslog_f.group == "root"
