e376b6f4751b2919f2269fb675209c2afeb0918b
juju4 <juju4@users.noreply.github.com>
2026-05-24 18:30:07 +0000

    fix: test_sshd with sudo

--- tests/test_sshd.py
+++ tests/test_sshd.py
@@ -1,7 +1,8 @@
 def test_sshdconf_file(host):
-    sshdconf_f = host.file("/etc/ssh/sshd_config")
-    assert sshdconf_f.contains("OpenBSD: sshd_config,")
-    assert sshdconf_f.contains("AllowAgentForwarding no")
-    assert sshdconf_f.user == "root"
-    assert sshdconf_f.group == "root"
-    assert sshdconf_f.mode == 0o644
+    with host.sudo():
+        sshdconf_f = host.file("/etc/ssh/sshd_config")
+        assert sshdconf_f.contains("OpenBSD: sshd_config,")
+        assert sshdconf_f.contains("AllowAgentForwarding no")
+        assert sshdconf_f.user == "root"
+        assert sshdconf_f.group == "root"
+        assert sshdconf_f.mode == 0o644
